ee2a813bb6d8d2d2582f070aa6edf0e09509e2a1
Tony Topol <atopol123@gmail.com>
2019-09-27 12:26:54 -0400

    the rest of the fucking owl

--- Assets/Scripts/HighScore.cs
+++ Assets/Scripts/HighScore.cs
@@ -7,10 +7,21 @@ public class HighScore : MonoBehaviour
 {
     public static int score = 1000;
 
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+            score = PlayerPrefs.GetInt("HighScore");
+
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+
     // Update is called once per frame
     void Update()
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "HighScore: " + score;
+
+        if (score > PlayerPrefs.GetInt("HighScore"))
+            PlayerPrefs.SetInt("HighScore", score);
     }
 }
